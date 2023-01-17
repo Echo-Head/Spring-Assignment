6754027073485bd303a7228a74b50c1a7cfa664a
HessianThespian <adrianfriduson@gmail.com>
2023-01-17 14:19:14 +0100

    Adrian: Removed an unused import from CustomerRepository

--- src/main/java/nav/fullstack/springdataapp/repositories/CustomerRepository.java
+++ src/main/java/nav/fullstack/springdataapp/repositories/CustomerRepository.java
@@ -1,6 +1,5 @@
 package nav.fullstack.springdataapp.repositories;
 
-import jakarta.transaction.Transactional;
 import nav.fullstack.springdataapp.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
